34fe07cac93dd9f71278e435ad1349b1f3d1f37c
marlonxlj <x@xlj.local>
2016-07-24 23:11:05 +0800

    添加工程

---
+++ XLCycleScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLCycleScrollView
+//
+//  Created by m on 16/7/24.
+//  Copyright © 2016年 XLJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XLCycleScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XLCycleScrollView
+//
+//  Created by m on 16/7/24.
+//  Copyright © 2016年 XLJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
